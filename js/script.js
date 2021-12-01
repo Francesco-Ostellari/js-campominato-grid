bcfd4b182e1e0cc442e347cd107e2ebca7a113cc
Francesco-Ostellari <francescoostellari@gmail.com>
2021-12-01 16:34:45 +0100

    prima-parte-js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,44 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 
+
+const buttonPlay = document.getElementById('play');
+console.log(buttonPlay);
+let containerGrid = document.getElementById('grid');
+console.log(containerGrid);
+
+
+buttonPlay.addEventListener('click', function () {
+  grid.innerHTML = '';
+  const level = document.getElementById('difficolta').value;
+  console.log(level);
+  let row = 0;
+  let col = 0;
+
+  if (level == 'easy') {
+    row = 10;
+    col = 10;
+  } else if (level == 'hard') {
+    row = 9;
+    col = 9;
+  } else if (level == 'crazy') {
+    row = 7;
+    col = 7;
+  }
+  let numberSquare = row * col;
+  for (let index = 0; index < numberSquare; index++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `calc(100% / ${col})`;
+    square.style.height = `calc(100% / ${row})`;
+    containerGrid.append(square);
+    console.log(square);
+
+    square.append(index + 1);
+
+    square.addEventListener('click', function () {
+      this.classList.add('active');
+    })
+  }
+  console.log(numberSquare);
+});
